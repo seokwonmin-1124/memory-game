1d460d73c2c8ae31c9b74cf23dca4e6ee2036437
seokwonmin-1124 <seokwonmin@naver.com>
2022-05-15 11:59:16 +0900

    memory.py 수정, 리드미 수정

--- memory.py
+++ memory.py
@@ -12,17 +12,21 @@ def mix(lev):
     answer = input("숫자 입력 : ")
     if int(answer) == list[0]:
         print("정답입니다.")
-    while count <= lev:
-        num = random.randint(0, 9)
-        num = int(num)
-        print(num)
-        list.append(num)
-        dap = ''.join(map(str, list))
-        answer = input("숫자 입력 : ")
-        if answer != dap:
-            print(f"정답은 {dap} 입니다")
-            print("오답")
-            break
-        else:
-            None
+        while count <= lev:
+            num = random.randint(0, 9)
+            num = int(num)
+            print(num)
+            list.append(num)
+            dap = ''.join(map(str, list))
+            answer = input("숫자 입력 : ")
+            if answer != dap:
+                print(f"정답은 {dap} 입니다")
+                print(f"{count} 단계까지 성공했습니다.")
+                print("오답")
+                break
+            else:
+                None
+            count += 1
+    else:
+        print("오답입니다. 처음부터 다시 시작합니다.")
 mix(10)
